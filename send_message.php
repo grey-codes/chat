f7b21040d2e323fda707968b0ed16d796f0ccb3c
Grey Ruessler <greyruessler@gmail.com>
2019-12-02 06:14:40 +0000

    profanity filter integrated

--- send_message.php
+++ send_message.php
@@ -2,7 +2,11 @@
 
 //use voku\helper\AntiXSS;
 use Sentiment\Analyzer;
+use mofodojodino\ProfanityFilter\Check;
+use Snipe\BanBuilder\CensorWords;
 
+$SWEAR_FILTER_MIN_SENTIMENT = -0.5;
+$SWEAR_FILTER_CENSOR_SENTIMENT = 0;
 
 include("shared.php");
 header('Content-Type: application/json');
@@ -39,11 +43,22 @@ $chid_safe=$cha->channel_id;
 $owner_id=$sessUser->user_id;
 
 
+$min_Sentiment=$cha->minSentiment;
+if ($min_Sentiment >= $SWEAR_FILTER_MIN_SENTIMENT) {
+    $check = new Check();
+    if ($check->hasProfanity($msg) && $min_Sentiment >= $SWEAR_FILTER_CENSOR_SENTIMENT) {
+        die("{\"success\":false,\"error\":\"Message does not follow channel rules - no profanity.\"}");
+    } else {
+        $censor = new CensorWords;
+        $string = $censor->censorString($msg);
+        $msg = $string["clean"];
+    }
+}
+
 $Parsedown = new Parsedown();
 $Parsedown->setSafeMode(true);
 $msg_markdown=$Parsedown->text($msg);
 
-$min_Sentiment=$cha->minSentiment;
 $analyzer = new Analyzer();
 $msgClean = strip_tags($msg_markdown);
 $vader_result = $analyzer->getSentiment($msgClean . " " . $msgClean);
