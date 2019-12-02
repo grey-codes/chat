38e3e22d1828e653cc82e052186839070449de5b
Grey Ruessler <greyruessler@gmail.com>
2019-12-02 01:44:42 +0000

    update getsent

--- getsent.php
+++ getsent.php
@@ -7,8 +7,14 @@ use Sentiment\Analyzer;
 include("shared.php");
 header('Content-Type: text/plain');
 
+$Parsedown = new Parsedown();
+$Parsedown->setSafeMode(true);
+$msg_markdown=$Parsedown->text($_POST["message"]);
+
 $analyzer = new Analyzer();
-$vader_result = $analyzer->getSentiment($_POST["message"]);
+$msgClean = strip_tags($msg_markdown);
+$vader_result = $analyzer->getSentiment($msgClean . " " . $msgClean);
+
 echo($_POST["message"] . "\n");
 var_dump($vader_result);
 ?>
